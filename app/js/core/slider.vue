a426bd547483698013a1282f3bf19b76cfc1b4e8
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-14 19:01:22 +0800

    init

---
+++ app/js/core/slider.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <section :class="cname">
+    <swiper :options="options" :notNextTick="options.notNextTick">
+      <swiper-slide v-for:item in items :key = "item.href">
+        <router-link :to="{name:item.href}">
+          <img :src="item.src" alt=""> 
+          <router-link/> 
+       </swiper-slide> 
+    </swiper>  
+
+  </section>   
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  components: {
+    swiper,
+    swiperSlide
+  },
+  props: {
+    cname: {
+      type: String,
+      default: ''
+    }
+  },
+  options: {
+    type: String,
+    default() {
+      return {
+        autoplay: true,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        notNextTick: false
+      }
+    }
+  },
+  items: {
+    type: Array,
+    default() {
+      return []
+    }
+  }
+}
+</script>
+
+<style>
+@import '~swiper/dost/css/swiper.css';
+</style>
