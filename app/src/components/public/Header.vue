8bf0874527a617de6e06162d3efad8de10e2f4c4
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-23 22:21:25 +0800

    完成两个组件

---
+++ app/src/components/public/Header.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+    <div class="header">
+        <span class="left">
+            <span>注册</span>&nbsp;|&nbsp;<span>登录</span>
+        </span>
+        <div class="btnDownload">APP下载</div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.header {
+  display: inline-block;
+  color: #666;
+  height: 5rem;
+  line-height: 5rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: #fff url(//m.jr.jd.com/spe/qyy/main/images/jr-logo.png) center
+    center no-repeat;
+  background-size: auto 50%;
+}
+.left {
+  font-size: 1rem;
+  height: 2rem;
+  color: #000;
+  line-height: 2rem;
+  margin: 1rem;
+}
+.btnDownload {
+  float: right;
+  font-size: 1rem;
+  padding: 0px 5px;
+  margin: 1.3rem 1rem 0 0;
+  height: 2.5rem;
+  width: 4rem;
+  line-height: 2.5rem;
+  min-width: 1rem;
+  border-radius: 4px;
+  background: #f40;
+  text-align: center;
+  color: #fff;
+}
+</style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home/index.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// import Money from '../money/index.vue'
+// import Ious from '../ious/index.vue'
+// import Raise from '../raise/index.vue'
+// import Download from '../special/download.vue'
+import 'swiper/dist/css/swiper.css'
 
 Vue.use(Router)
+Vue.use(VueAwesomeSwiper)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
     }
   ]
 })
+
+// , {
+//   path: "/money",
+//   name: "money",
+//   component: Money,
+// }, {
+//   path: "/ious",
+//   name: "ious",
+//   component: Ious,
+// }, {
+//   path: "/raise",
+//   name: "raise",
+//   component: Raise,
+// }, {
+//   path: "/download",
+//   name: "download",
+//   component: Download,
+// },
