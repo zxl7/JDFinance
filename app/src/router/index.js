a8ca9e01eebbccfccf7f00a99b391648a4578da7
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-24 00:12:37 +0800

    Update index.js

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home/index.vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 // import Money from '../money/index.vue'
 // import Ious from '../ious/index.vue'
 // import Raise from '../raise/index.vue'
 // import Download from '../special/download.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 
 Vue.use(Router)
