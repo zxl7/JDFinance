73323ffb1ae176a898052205846166709154f51a
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-24 17:44:48 +0800

    Create adc.vue

---
+++ app/src/components/money/adc.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <Panel title="京东智投" class="panel">
+        <section class="content">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t7162/219/4755741/18941/4e51a5aa/597b066dNf6c7a972.jpg?width=750&height=120" alt="">
+        </section>
+    </Panel>
+</template>
+
+<script>
+import Panel from '../core/panel.vue'
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+<style scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
