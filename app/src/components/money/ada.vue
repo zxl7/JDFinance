11e0e45e60ec10d5d01fa4053cc26036672f0aa6
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-24 17:44:46 +0800

    Create ada.vue

---
+++ app/src/components/money/ada.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <Panel title="优惠神劵" class="panel">
+        <section class="content">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t5842/235/8356236245/38364/2112fd7e/597a95d4Ne01da140.png?width=750&height=270" alt="">
+        </section>
+    </Panel>
+</template>
+
+<script>
+import Panel from '../core/panel.vue'
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+.content img {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+</style>
