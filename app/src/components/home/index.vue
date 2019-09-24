7d29dd5027b77ff6eb9cfe23d0beb14a89b26002
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-24 16:19:57 +0800

    完成首页Home页面

--- app/src/components/home/index.vue
+++ app/src/components/home/index.vue
@@ -3,37 +3,36 @@
         <Heador/>
         <Slider/>
         <Novice/>
-        <!--
         <Borrow/>
         <Money/>
         <Product/>
         <Life/>
         <Footor/>
-        <Navbar/> -->
+        <Navbar/>
     </div>
 </template>
 
 <script>
-import Heador from '../public/Header.vue'
+import Heador from '../public/header.vue'
 import Slider from './hslider.vue'
+import Borrow from './borrow.vue'
 import Novice from './novice.vue'
-// import Footor from '../public/footer.vue'
-// import Navbar from '../public/navbar.vue'
-// import Borrow from './borrow.vue'
-// import Money from './money.vue'
-// import Product from './product.vue'
-// import Life from './life.vue'
+import Money from './money.vue'
+import Product from './product.vue'
+import Life from './life.vue'
+import Footor from '../public/footer.vue'
+import Navbar from '../public/navbar.vue'
 export default {
   components: {
     Heador,
     Slider,
-    Novice
-    // Borrow,
-    // Money,
-    // Product,
-    // Life,
-    // Footor,
-    // Navbar
+    Novice,
+    Borrow,
+    Money,
+    Product,
+    Life,
+    Footor,
+    Navbar
   }
 }
 </script>
