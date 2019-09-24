068853f811cc2e4364e74a04833ba87fd39af086
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-24 16:19:25 +0800

    Create borrow.vue

---
+++ app/src/components/home/borrow.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+    <Panel title="极速借贷" class="panel">
+        <section class="content">
+            <router-link :to="{ name: 'home' }">
+                <img src="//img12.360buyimg.com/jrpmobile/jfs/t12721/42/2497849749/26333/ec584be4/5a421756N2416c88f.png?width=750&height=280" alt="">
+            </router-link>
+        </section>
+    </Panel>
+</template>
+
+<script scoped>
+import Panel from '../core/panel.vue'
+
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style>
+.panel {
+  text-align: center;
+}
+
+.content {
+  padding-bottom: 30px;
+}
+img {
+  width: 100%;
+}
+</style>
