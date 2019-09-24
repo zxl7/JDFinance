d4cc67de7fb309a02a95ba4bcb4fbe733b968157
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-24 17:44:50 +0800

    Create adb.vue

---
+++ app/src/components/money/adb.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <Panel title="理财专享" class="panel">
+        <section class="content">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t10876/23/2566835181/40527/d7d27c08/59f97d68Ne53fda5b.png?width=1080&height=240" alt="">
+        </section>
+    </Panel>
+</template>
+
+<script>
+import Panel from '../core/panel.vue'
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+.content img {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+</style>
