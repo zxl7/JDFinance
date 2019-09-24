20cd75d88446dfb415a0e1cdda35805b02305875
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-24 16:19:44 +0800

    Update Header.vue

--- app/src/components/public/Header.vue
+++ app/src/components/public/Header.vue
@@ -1,16 +1,25 @@
 <template lang="html">
+  <div class= "heador">
     <div class="header">
         <span class="left">
             <span>注册</span>&nbsp;|&nbsp;<span>登录</span>
         </span>
         <div class="btnDownload">APP下载</div>
     </div>
+  </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
+.heador {
+  z-index: 100px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
 .header {
   display: inline-block;
   color: #666;
@@ -22,7 +31,7 @@
   background: #fff url(//m.jr.jd.com/spe/qyy/main/images/jr-logo.png) center
     center no-repeat;
   background-size: auto 50%;
-  position: fixed;
+  position: absolute;
 }
 .left {
   font-size: 1rem;
@@ -35,7 +44,7 @@
   float: right;
   font-size: 1rem;
   padding: 0px 5px;
-  margin: 1.3rem 1rem 0 0;
+  margin: 1.3rem 0.8rem 0 0;
   height: 2.5rem;
   width: 4rem;
   line-height: 2.5rem;
